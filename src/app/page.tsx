68142ad681804bddb47c27bae752a776740a4aac
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-10 15:47:45 +0530

    Add: content and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,61 @@
 import Image from 'next/image'
 import pfp from '../../public/profile.jpg'
+import { BsChevronDoubleDown } from 'react-icons/bs';
 
 export default function Home() {
   const imageStyle = {
     borderRadius: '50%',
     border: '1px solid #fff',
+
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className='flex flex-col items-center mx-auto bg-Theme-black rounded-xl shadow-md overflow-hidden md:max-w-4xl p-8'>
-        <div className='grid grid-cols-2 gap-4 items-center'>
+        <div className='flex flex-row items-center gap-40'>
           <div>
           <Image
             src="/favicon.ico"
-            width={30}
-            height={30}
-            alt="Dilip Singh Gurjar"
+            width={40}
+            height={40}
+            alt="Advocate"
             style={imageStyle}
           />
           </div>
-          <div><button className="btn btn-outline">Button</button></div>
+          <div className='flex flex-row gap-2'>
+            <button className="btn btn-outline">book a session</button>
+          </div>
         </div>
-        <div className='grid grid-row-3 gap-4 p-8'>
-          <div className='flex flex-col items-center text-white'>
+        <div className='grid grid-row-3 gap-6 p-6'>
+          <div className='flex flex-col items-center text-white text-2xl'>
             <h3 >Advocate</h3>
             <h1 className='text-decoration-solid'>DILIP SINGH GURJAR</h1>
           </div>
           <div className='flex flex-col items-center'>
           <Image
             src={pfp}
-            width={200}
-            height={300}
+            width={400}
+            height={500}
             alt="Dilip Singh Gurjar"
             style={imageStyle}
           />
           </div>
-          <div className='flex flex-col items-center text-white text-3xl'>
+          <div className='flex flex-col items-center text-white text-2xl'>
             <h3 >Speacialist</h3>
-            <h1>Criminal law</h1>
+            <h1 className=' text-3xl'>Criminal law</h1>
           </div>
         </div>
-        <div className='flex flex-col justify-center'>
-          <div>btm-t</div>
-          <div>btm-b</div>
+        <div className='flex flex-col justify-center items-center gap-6'>
+          <div className='text-xl'>
+            <p>
+              I am <b>Dilip Singh Gurjar</b>.
+              <br />
+              I have been practicing Law and have experience of more than a Decade.
+              I like to build trust with my clients and help them get their JUSTICE.
+              <br />
+              Lorem ipsum dolor sit amet consect, adipisicing elit.
+              </p>
+          </div>
+          <div><button className='btn btn-neutral'><BsChevronDoubleDown/>see more</button></div>
         </div>
       </div>
     </main>
