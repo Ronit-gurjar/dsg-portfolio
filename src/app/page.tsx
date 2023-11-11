4789135576c0e771c28077a21c8ce407b64a3334
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-12 01:26:53 +0530

    Add: responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <div className='flex flex-col items-center mx-auto bg-Theme-black rounded-xl shadow-md overflow-hidden md:max-w-4xl p-8'>
-        <div className='flex flex-row items-center gap-40'>
+      <div className='flex flex-col items-center mx-auto bg-Theme-black rounded-xl shadow-md overflow-hidden p-8'>
+        <div className='flex flex-row items-center gap-20 lg:gap-40 lg:justify-left'>
           <div>
           <Image
             src="/favicon.ico"
@@ -24,28 +24,28 @@ export default function Home() {
             <button className="btn btn-outline">book a session</button>
           </div>
         </div>
-        <div className='grid grid-row-3 gap-6 p-6'>
-          <div className='flex flex-col items-center text-white text-2xl'>
+        <div className='flex flex-col lg:flex-row lg:items-center mt-8 gap-10 p-11'>
+          <div className='flex flex-col items-center text-white text-2xl lg:text-6xl'>
             <h3 >Advocate</h3>
-            <h1 className='text-decoration-solid'>DILIP SINGH GURJAR</h1>
+            <h1 className='text-3xl'>DILIP SINGH GURJAR</h1>
           </div>
           <div className='flex flex-col items-center'>
           <Image
             src={pfp}
             width={400}
-            height={500}
+            height={400}
             alt="Dilip Singh Gurjar"
             style={imageStyle}
           />
           </div>
-          <div className='flex flex-col items-center text-white text-2xl'>
+          <div className='flex flex-col items-center text-white text-2xl lg:text-6xl'>
             <h3 >Speacialist</h3>
-            <h1 className=' text-3xl'>Criminal law</h1>
+            <h1 className='text-3xl'>Criminal law</h1>
           </div>
         </div>
         <div className='flex flex-col justify-center items-center gap-6'>
           <div className='text-xl'>
-            <p>
+            <p className='text-center text-2xl'>
               I am <b>Dilip Singh Gurjar</b>.
               <br />
               a dedicated advocate with over a decade of experience practicing law. 
