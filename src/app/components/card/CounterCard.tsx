73c6bd6b3394d73949952efee81444ca5c6789ce
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-12 19:26:46 +0530

    Fix: add '+' symbol

--- src/app/components/card/CounterCard.tsx
+++ src/app/components/card/CounterCard.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { type } from 'os'
 import React from 'react'
 import CountUp from 'react-countup'
 
@@ -11,9 +10,9 @@ type CounterCardProps={
 const CounterCard= ({num, title}:CounterCardProps) => {
   const countTo = num;
   return (
-    <div className='bg-Theme-black rounded-md p-8 flex flex-col items-center'>
+    <div className='bg-Theme-black rounded-md p-8 flex flex-col items-center text-white'>
        <h1 className='text-5xl lg:text-9xl'>
-        <CountUp end={countTo}/>
+        +<CountUp end={countTo}/>
        </h1>
        <h3 className='lg:text-5xl'>{title}</h3>
     </div>
