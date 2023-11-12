f405abbcf3fe0d80d30887a258a3b6409d62d9e5
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-12 19:29:04 +0530

    Create: LabelCard component

---
+++ src/app/components/card/LabelCard.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { type } from 'os'
+import React from 'react'
+import CountUp from 'react-countup'
+
+type LabelCardProps={
+    label: string;
+}
+const LabelCard= ({label}:LabelCardProps) => {
+  return (
+    <div className='bg-Theme-black rounded-md p-8 flex flex-col items-center text-white'>
+       <h1 className='text-5xl lg:text-9xl'>
+         {label}
+       </h1>
+    </div>
+  )
+}
+
+export default LabelCard
