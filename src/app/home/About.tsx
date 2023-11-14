af04694ca3805a0abd511d40698f69759382fe10
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-14 21:02:45 +0530

    Add: About component

---
+++ src/app/home/About.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Image from 'next/image'
+import figure from '../../../public/lady justice.png'
+import CounterCard from '../components/card/CounterCard'
+
+const About = () => {
+  return (
+    <div className='flex flex-col item-center justify-center gap-4 p-2'>
+        <div><h1 className='text-4xl text-Theme-black text-center underline underline-offset-4 font-serif p-2'>Experience</h1></div>
+        <div className='flex flex-col lg:flex-row lg:justify-center gap-4'>
+            <CounterCard num={1700} title='Total Cases'/>
+            <CounterCard num={1009} title='Pending Cases'/>
+            <CounterCard num={746} title='Desposed Cases'/>
+        </div>
+        <div className='flex flex-col lg:flex-row justify-center gap-8'>
+            <div className='w-fit'>
+                <Image src={figure} alt='justice' width={700} height={500}/>
+            </div>
+            <div className='w-96'>
+                <p className='text-2xl text-Theme-black bg-Theme-cream -mt-48 lg:mt-0 lg:p-10'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Maxime dolorum, sequi sint explicabo placeat et ullam exercitationem.
+                     Ut iste necessitatibus quia, praesentium magni recusandae totam dolore, voluptatibus ex, corrupti libero!
+                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Maxime dolorum, sequi sint explicabo placeat et ullam exercitationem.
+                   
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
