cae172645862376209815c478175ad1965e0d9ce
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-16 00:11:45 +0530

    Fix: typo

--- src/app/components/card/LabelCard.tsx
+++ src/app/components/card/LabelCard.tsx
@@ -9,8 +9,8 @@ type LabelCardProps={
 }
 const LabelCard= ({label}:LabelCardProps) => {
   return (
-    <div className='bg-Theme-black rounded-md p-8 flex flex-col items-center text-white'>
-       <h1 className='text-5xl lg:text-9xl'>
+    <div className='bg-Theme-black rounded-md p-4 w-96 flex flex-col items-center text-white'>
+       <h1 className='text-5xl'>
          {label}
        </h1>
     </div>
