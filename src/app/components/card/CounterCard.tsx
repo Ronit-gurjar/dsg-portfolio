c3744feb3de7947ba42dbad03ba9ce08eda8dba9
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-15 13:02:59 +0530

    Add: feature to add icon

--- src/app/components/card/CounterCard.tsx
+++ src/app/components/card/CounterCard.tsx
@@ -6,13 +6,15 @@ import CountUp from 'react-countup'
 type CounterCardProps={
     num: number;
     title: string;
+    icon: JSX.Element;
 }
-const CounterCard= ({num, title}:CounterCardProps) => {
+const CounterCard= ({num, title, icon}:CounterCardProps) => {
   const countTo = num;
   return (
     <div className='bg-Theme-black rounded-md p-8 flex flex-col items-center text-white'>
-       <h1 className='text-5xl lg:text-9xl'>
-        +<CountUp end={countTo}/>
+
+       <h1 className='text-5xl flex lg:text-9xl'>
+        {icon}<CountUp end={countTo}/>
        </h1>
        <h3 className='lg:text-5xl'>{title}</h3>
     </div>
