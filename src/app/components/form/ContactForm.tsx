8ae6c1aa0590270311195829301eb7c5582d2806
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-17 01:07:56 +0530

    REFACTOR✅

--- src/app/components/form/ContactForm.tsx
+++ src/app/components/form/ContactForm.tsx
@@ -1,23 +1,18 @@
 'use client'
 
-import { sendEmail } from '@/app/api/send/route'
 import React from 'react'
 import {BsFillSendFill} from 'react-icons/bs'
 
 const ContactForm = () => {
   return (
-    <form className='bg-Theme-black w-96 lg:w-full h-fit flex flex-col relative top-0 rounded-md p-8' action={async (formData) =>{
-        await sendEmail(formData)
-    }}>
+    <form className='bg-Theme-black w-96 lg:w-full h-fit flex flex-col items-center justify-center relative top-0 rounded-md p-8'>
         <h1 className='text-3xl font-serif p-8 bg-Theme-cream text-Theme-black rounded-md'>Request a Free Quote</h1>
         <div className='flex flex-col gap-4 p-4'>
-        <input name='Name' type="text" required placeholder="Name" className="input lg:input-lg input-bordered w-full max-w-xs" />
-        <input name='Email' type="text" required placeholder="Email" className="input lg:input-lg input-bordered w-full max-w-xs" />
-        <textarea className="textarea lg:textarea-lg textarea-bordered" name='Message' required placeholder="Message"></textarea>
-        <div className='flex items-center justify-between'>
-            <div>we will relpy <br/>as soon as possible.</div>
-            <button className='btn btn-outline' type='submit'><BsFillSendFill/>Send</button>
-        </div>
+        <h1 className="flex items-center justify-center input lg:input-lg input-bordered w-full max-w-xs">+91 9926215979</h1>
+        <h1 className="flex items-center justify-center input lg:input-lg input-bordered w-full max-w-xs">+91 9424060563</h1>
+        <h1 className="flex items-center justify-center input lg:input-lg input-bordered w-full max-w-xs">gurjardilip1984advo@gmail.com</h1>
+        <h1 className="flex items-center justify-center text-center input lg:input-lg input-bordered w-full max-w-xs">A-380, Swapna city, freegunj, Shujalpur</h1>
+        <p className='flex flex-col items-center justify-center'><div className='text-2xl font-extrabold'>Office Time</div><div>Morning 08:00 AM to 10:00 AM</div><div>Evening 07:00 PM to 09:00 PM</div></p>
         </div>
     </form>
   )
