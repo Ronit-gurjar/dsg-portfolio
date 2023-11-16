51f149b69a2d61967fe7527b67d5975f814cda46
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-11-17 01:21:51 +0530

    REFACTOR✅

--- src/app/home/Footer.tsx
+++ src/app/home/Footer.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react"
 
-import TiltImageCard from '../components/card/TiltImageCard'
-import IconButton from '../components/button/IconButton'
+import TiltImageCard from "../components/card/TiltImageCard"
+import IconButton from "../components/button/IconButton"
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaWhatsappSquare } from "react-icons/fa";
@@ -9,34 +9,34 @@ import { MdCopyright } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <div className='p-4 mt-7 lg:mt-14'>
-        <div className='flex flex-col items-center mx-auto bg-Theme-black rounded-xl shadow-md overflow-hidden p-8'>
-        <div className='flex flex-col lg:flex-row lg:items-center mt-1 lg:mt-10 gap-10 p-6 lg:p-10'>
-          <div className='flex flex-col items-center text-white text-2xl lg:text-6xl'>
+    <div className="p-4 mt-7 lg:mt-14">
+        <div className="flex flex-col items-center mx-auto bg-Theme-black rounded-xl shadow-md overflow-hidden p-8">
+        <div className="flex flex-col lg:flex-row lg:items-center mt-1 lg:mt-10 gap-10 p-6 lg:p-10">
+          <div className="flex flex-col items-center text-white text-2xl lg:text-6xl">
             <h3 >Advocate</h3>
-            <h1 className='text-2xl lg:text-4xl'>DILIP SINGH GURJAR</h1>
+            <h1 className="text-2xl lg:text-4xl">DILIP SINGH GURJAR</h1>
           </div>
-          <div className='flex flex-col items-center'>
+          <div className="flex flex-col items-center">
             <TiltImageCard/>
           </div>
-          <div className='flex flex-col items-center text-white text-2xl lg:text-6xl'>
-            <h3 >Let's Connnect</h3>
-            <div className='text-5xl flex gap-5 lg:text-9xl p-6'>
-                <IconButton icon={<FaFacebookSquare />} url='https://www.facebook.com/dilipsingh.gurjar.92?mibextid=ZbWKwL'/>
-                <IconButton icon={<FaInstagramSquare />} url='https://instagram.com/gurjar.dilip1984?igshid=OGQ5ZDc2ODk2ZA=='/>
-                <IconButton icon={<FaWhatsappSquare />} url='https://wa.me/qr/FPM32MVPYENEE1'/>
+          <div className="flex flex-col items-center text-white text-2xl lg:text-6xl">
+            <h3 >Let"s Connnect</h3>
+            <div className="text-5xl flex gap-5 lg:text-9xl p-6">
+                <IconButton icon={<FaFacebookSquare />} url="https://www.facebook.com/dilipsingh.gurjar.92?mibextid=ZbWKwL"/>
+                <IconButton icon={<FaInstagramSquare />} url="https://instagram.com/gurjar.dilip1984?igshid=OGQ5ZDc2ODk2ZA=="/>
+                <IconButton icon={<FaWhatsappSquare />} url="https://wa.me/qr/FPM32MVPYENEE1"/>
             </div>
           </div>
         </div>
-        <div className='flex flex-col justify-center items-center gap-4'>
-          <div className='p-8'>
-            <p className='text-center text-Theme-cream text-5xl lg:text-8xl'>
+        <div className="flex flex-col justify-center items-center gap-4">
+          <div className="p-8">
+            <p className="text-center text-Theme-cream text-5xl lg:text-8xl">
               <b>न्यायः मम धर्मः</b>
               </p>
           </div>
-          <div className='flex gap-6'>
-            <p className='text-Theme-cream flex gap-1 items-center'>All rights reserved <MdCopyright /></p>
-            <button className='btn btn-outline text-Theme-cream'><a href="https://github.com/Ronit-gurjar">Made by Ronit </a></button>
+          <div className="flex gap-6">
+            <p className="text-Theme-cream flex gap-1 items-center">All rights reserved <MdCopyright /></p>
+            <button className="btn btn-outline text-Theme-cream"><a href="https://github.com/Ronit-gurjar">Made by Ronit </a></button>
           </div>
         </div>
       </div>
--- src/app/home/Team.tsx
+++ src/app/home/Team.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import ImageCard from '../components/card/ImageCard'
-import team1 from '../../../public/team1.jpeg'
-import team2 from '../../../public/team2.jpeg'
-import team3 from '../../../public/team3.jpeg'
+import React from "react"
+import ImageCard from "../components/card/ImageCard"
+import team1 from "../../../public/team1.jpeg"
+import team2 from "../../../public/team2.jpeg"
+import team3 from "../../../public/team3.jpeg"
 
 const Team = () => {
   return (
-    <div className='bg-Theme-black w-full h-fit flex flex-col items-center justify-center gap-8 lg:p-8 rounded-lg'>
+    <div className="bg-Theme-black w-full h-fit flex flex-col items-center justify-center gap-8 lg:p-8 rounded-lg">
         <div>
-            <h1 className='text-4xl text-Theme-cream text-center underline underline-offset-4 font-serif p-4'>Team</h1>
-            <h1 className='text-2xl'><strong>Meet our vibrant team</strong></h1>
+            <h1 className="text-4xl text-Theme-cream text-center underline underline-offset-4 font-serif p-4">Team</h1>
+            <h1 className="text-2xl"><strong>Meet our vibrant team</strong></h1>
         </div>
-        <div className='flex flex-col gap-4 lg:gap-40 lg:flex-row items-center'>
+        <div className="flex flex-col gap-4 lg:gap-40 lg:flex-row items-center">
         
-            <p className='lg:hidden'>(click on image to see info)</p>
-            <ImageCard imageSrc={team1} name='Kush Upadhyay' title='Advocate' experience={5} contact={8305841391}/>
-            <ImageCard imageSrc={team2} name='Lokesh Rajak' title='Advocate' experience={3} contact={9165506790}/>
-            <ImageCard imageSrc={team3} name='Istiyak Ansari' title='Advocate' experience={3} contact={8821847419}/>
+            <p className="lg:hidden">(click on image to see info)</p>
+            <ImageCard imageSrc={team1} name="Kush Upadhyay" title="Advocate" experience={5} contact={8305841391}/>
+            <ImageCard imageSrc={team2} name="Lokesh Rajak" title="Advocate" experience={3} contact={9165506790}/>
+            <ImageCard imageSrc={team3} name="Istiyak Ansari" title="Advocate" experience={3} contact={8821847419}/>
         </div>
         <div>
-            <p className='p-4 text-2xl text-center mt-2'>
+            <p className="p-4 text-2xl text-center mt-2">
             a dynamic ensemble of young, passionate advocates each wielding great amount of invaluable experience.
             Committed to innovation and excellence, our team combines youthful energy with seasoned expertise,
-            dedicated to championing our clients' causes with unwavering zeal and fresh perspectives.
+            dedicated to championing our clients" causes with unwavering zeal and fresh perspectives.
             </p>
         </div>
     </div>
